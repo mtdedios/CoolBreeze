d6c149ef5c6fe01d10f030562fe2ae6f269f91ee
DE DIOS <mtdedios@mediaquest.com.ph>
2018-08-28 15:48:38 +0800

    Add project files.

---
+++ CoolBreeze/CoolBreeze/SplashPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoolBreeze
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SplashPage : ContentPage
+	{
+		public SplashPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ImagePage());
+        }
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new PhonePage());
+        }
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridLogin());
+        }
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new QuotesPage());
+        }
+    }
+}
